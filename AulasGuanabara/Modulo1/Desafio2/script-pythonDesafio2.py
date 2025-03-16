c6dbdf0eb7be4a370227f8ef9037b442d8ac4d04
Marcelo Henrique Favaro <130109482+MarceloTIFavaro@users.noreply.github.com>
2025-03-16 16:10:34 -0300

    aula1

---
+++ AulasGuanabara/Modulo1/Desafio2/script-pythonDesafio2.py
@@ -0,0 +1,4 @@
+dia =input('Qual o dia de seu nascimento? ')
+mes =input('Qual o mês de seu nascimento? ')
+ano=input('Qual o ano de seu nascimento? ')
+print('Você nasceu no dia',dia,'de',mes,'de',ano,'. Corretor?')
