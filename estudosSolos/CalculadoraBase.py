7dd1547147020b0d0c7e2b73ca316381b5d2ac52
Marcelo Henrique Favaro <130109482+MarceloTIFavaro@users.noreply.github.com>
2025-03-19 18:40:39 -0300

    CalculadoraConcluida

--- estudosSolos/CalculadoraBase.py
+++ estudosSolos/CalculadoraBase.py
@@ -1,21 +1,55 @@
-num1 = int(input('Digite o primeiro número: '))
-num2 = int(input('Digite o segundo número: '))
+def calculate():
+    operation = input( '''
+Por favor, digite a operação matemática que você gostaria de concluir:
++ para adição
+- para subtração
+* para multiplicação
+/ para divisão
+''')
 
-operation = input(
-    '''
-    Por favor, digite a operação matemática que você gostaria de concluir:
-    + para adição
-    - para subtração
-    * para multiplicação
-    / para divisão
-    '''
-
-)
-
-adicao = num1 + num2
-subtracao = num1 - num2
-multiplicacao =  num1 * num2
-divisao = num1 / num2
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
 
 
-print('{} + {} = {}'.format(num1, num2, adicao))
+    adicao = num1 + num2
+    subtracao = num1 - num2
+    multiplicacao =  num1 * num2
+    divisao = num1 / num2
+
+    if operation == '+':
+        print('{} + {} = {}'.format(num1, num2, adicao))
+
+    elif operation == '-':
+        print('{} + {} = {}'.format(num1, num2, subtracao))
+
+    elif operation == '*':
+        print('{} + {} = {}'.format(num1, num2, multiplicacao))
+
+    elif operation == '/':
+        print('{} + {} = {}'.format(num1, num2, divisao))
+
+    else:
+        print('Por favor, escolha uma operação valida ' 
+              'Execute o programa novamente')
+
+    #adicionando a função again()
+    again()
+
+def again():
+    calc_again = input( '''
+Quer calcular novamente? Por favor, digite Y para SIM e N para NÃO
+''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('Até mais..bye')
+    else:
+        again()
+
+def welcome():
+    print('''
+Bem vindo a minha calculadora
+    ''')
+welcome()
+calculate()
